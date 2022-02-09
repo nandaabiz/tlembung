b0c2791b2d9a7dd132df608908e8e0108975d4cb
Nanda Widyatama <nanda7anku@gmail.com>
2022-02-09 09:39:58 +0700

    All fix

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -5,13 +5,15 @@ import { GAME_TITLE } from '../constants/strings'
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
     `${GAME_TITLE} %23${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
-      generateEmojiGrid(guesses, '\n')
+      generateEmojiGrid(guesses, '\n') + '\n\n' +
+      'https://tlembung.vercel.app/'
   )
 }
 export const shareTwitter = (guesses: string[], lost: boolean) => {
   let url = 'https://twitter.com/intent/tweet?text=' +
     `${GAME_TITLE} %23${solutionIndex} ${lost ? 'X' : guesses.length}/6%0A%0A` +
-    generateEmojiGrid(guesses, '%0A')
+    generateEmojiGrid(guesses, '%0A') + '%0A%0A' +
+    'https://tlembung.vercel.app/'
   const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
   if (newWindow) newWindow.opener = null
 }
