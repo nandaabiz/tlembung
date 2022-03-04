287649127cc830561f239c99fad72f017ebd3fc1
Nanda Widyatama <nanda7anku@gmail.com>
2022-03-04 15:12:54 +0700

    Valid wordlist updated

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -4,7 +4,7 @@ import { GAME_TITLE } from '../constants/strings'
 
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
-    `${GAME_TITLE} %23${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
+    `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
       generateEmojiGrid(guesses, '\n') + '\n\n' +
       'https://tlembung.vercel.app/'
   )
